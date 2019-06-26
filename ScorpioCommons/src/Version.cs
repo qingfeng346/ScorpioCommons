74ff6a968aa71dd8adabc33ee98bda4d34125ddb
linyuan.yang <linyuan.yang@centurygame.com>
2019-06-26 16:56:19 +0800

    json 解析库

--- ScorpioCommons/src/Version.cs
+++ ScorpioCommons/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.Commons {
     public static class Version {
-        public const string version = "1.0.10";
+        public const string version = "1.0.11";
         public const string date = "2019-06-26";
     }
 }
