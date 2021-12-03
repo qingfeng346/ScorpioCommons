4400cd64c761165be93da0dd353a800216d157c9
linyuan.yang <linyuan.yang@centurygame.com>
2021-12-03 17:02:51 +0800

    update

--- build.ps1
+++ build.ps1
@@ -21,6 +21,6 @@ dotnet pack -p:PackageVersion=$version -o ../bin
 
 Set-Location ../bin
 $apiKey = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("b3kyZDVxNmEyenZ1enlsZm5rcHFhaGU0eGlxMnJuZHViY2ZuMmQ2MnE3YmoyYQ=="))
-$nupkgName = "ScorpioCommons.$version.nupkg"
+$nupkgName = "Scorpio.Commons.$version.nupkg"
 dotnet nuget push $nupkgName -k $apiKey -s https://api.nuget.org/v3/index.json
 Set-Location ../
