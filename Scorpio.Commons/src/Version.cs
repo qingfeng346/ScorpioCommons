f8b477ad24e59f4f416d614f0949d20f53ae416a
while <qingfeng346@fengyuezhu.com>
2021-12-06 15:02:12 +0800

    Version

--- Scorpio.Commons/src/Version.cs
+++ Scorpio.Commons/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.Commons {
     public static class Version {
-        public const string version = "1.1.9";
-        public const string date = "2021-11-29";
+        public const string version = "1.1.11";
+        public const string date = "2021-12-06";
     }
 }
