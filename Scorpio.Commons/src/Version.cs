982a3027dddcfe9a88def11494b41ace6bc67754
linyuan.yang <linyuan.yang@centurygame.com>
2022-05-05 14:32:47 +0800

    update

--- Scorpio.Commons/src/Version.cs
+++ Scorpio.Commons/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.Commons {
     public static class Version {
-        public const string version = "1.1.16";
-        public const string date = "2022-03-14";
+        public const string version = "1.1.17";
+        public const string date = "2022-05-05";
     }
 }
