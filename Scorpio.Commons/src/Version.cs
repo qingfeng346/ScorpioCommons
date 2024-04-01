f391cb0cfbc1444392e83c86fca09c874bec08cb
linyuan.yang <linyuan.yang@centurygame.com>
2024-04-01 15:22:53 +0800

    fix

--- Scorpio.Commons/src/Version.cs
+++ Scorpio.Commons/src/Version.cs
@@ -1,6 +1,6 @@
 namespace Scorpio.Commons {
     public static class Version {
-        public const string version = "1.1.27";
-        public const string date = "2024-03-30";
+        public const string version = "1.1.28";
+        public const string date = "2024-04-01";
     }
 }
